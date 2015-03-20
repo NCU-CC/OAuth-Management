12c1d5c38b6bd846c6dbb0dba320a26bf3cfafc6
Yeh-Yung <yyc1217@gmail.com>
2015-03-20 21:36:05 +0800

    fail on jetty, success on tomcat, WTF?

--- src/main/java/tw/edu/ncu/cc/manage/service/login/IPersonService.java
+++ src/main/java/tw/edu/ncu/cc/manage/service/login/IPersonService.java
@@ -7,7 +7,7 @@ import tw.edu.ncu.cc.manage.entity.oauth.User;
 import tw.edu.ncu.cc.manage.service.IService;
 
 public interface IPersonService<T extends Person> extends IService<T>{
-    public static final String SERVICEURL = "https://127.0.0.1/oauth/management/v1/user/";
+    public static final String SERVICEURL = "https://api.cc.ncu.edu.tw/oauth/management/v1/user/";
     T findPersonByAccount(String account);
     Person getNewLoginPerson(HttpServletRequest request,String id);
     User createUserOnRemoteServer(String id);
--- src/main/java/tw/edu/ncu/cc/manage/service/login/PersonServiceImpl.java
+++ src/main/java/tw/edu/ncu/cc/manage/service/login/PersonServiceImpl.java
@@ -3,6 +3,7 @@ package tw.edu.ncu.cc.manage.service.login;
 import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.sql.Timestamp;
 import java.util.Date;
 import java.util.List;
 
@@ -20,58 +21,59 @@ import tw.edu.ncu.cc.manage.service.oauth.connector.Connection;
 import tw.edu.ncu.cc.manage.service.oauth.converter.UserConverter;
 
 @Service
-public class PersonServiceImpl <T extends Person> extends ServiceImpl<T> implements IPersonService<T>{
-    public Connection connection;
-    public PersonServiceImpl() {
-        connection= new Connection();
-    }
-    @SuppressWarnings("unchecked")
-    @Transactional(propagation=Propagation.SUPPORTS,readOnly=true)
-    public T findPersonByAccount(String account) {
-        List<T> person = this.getDao().creatQuery(
-                "select p from Person p " +
-                        "where p.account = :account and deleted=false "
-                        ).setParameter("account", account.trim()).getResultList();
-        if(person.size()>0){
-            return person.get(0);
-        }
-        return null;
-    }
+public class PersonServiceImpl<T extends Person> extends ServiceImpl<T> implements IPersonService<T> {
+	public Connection connection;
 
-    @Override
-    @Transactional(propagation = Propagation.REQUIRED, readOnly = false)
-    public void create(T person) {
-        if(findPersonByAccount(person.getAccount())!=null){
-            throw new RuntimeException("account " +person.getAccount() + "has already existed");
-        }
-        this.getDao().create(person);
-    }
+	public PersonServiceImpl() {
+		connection = new Connection();
+	}
 
-    public Person getNewLoginPerson(HttpServletRequest request,String id) {
-        Person person= new Person();
-        person.setAccount(id);
-        person.setDateCreated(new Date());
-        person.setDateLastActived(new Date());
-        person.setDeleted(false);
-        person.setIpCreated(request.getRemoteAddr());
-        person.setIpLastActived(request.getRemoteAddr());
-        person.setType(PersonType.STUDENT.toString());
-        return person;
-    }
+	@SuppressWarnings("unchecked")
+	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
+	public T findPersonByAccount(String account) {
+		List<T> person = this.getDao().creatQuery("select p from Person p " + "where p.account = :account and deleted=false ").setParameter("account", account.trim())
+				.getResultList();
+		if (person.size() > 0) {
+			return person.get(0);
+		}
+		return null;
+	}
 
-    public User createUserOnRemoteServer(String id) {
-        User user = null;
-        try {
-            HttpURLConnection connectionURL=connection.doConnection(new URL(SERVICEURL), UserConverter.convert(new User(id)), Connection.POST);
-            int status=connectionURL.getResponseCode();
-            connectionURL.connect();
-            if(status==200){
-                user= UserConverter.convert(connection.getStringFromConnection(connectionURL));
-            }            
-        } catch (IOException e) {
-            user=null;
-        }
-        return user;
-    }
+	@Override
+	@Transactional(propagation = Propagation.REQUIRED, readOnly = false)
+	public void create(T person) {
+		if (findPersonByAccount(person.getAccount()) != null) {
+			throw new RuntimeException("account " + person.getAccount() + "has already existed");
+		}
+		this.getDao().create(person);
+	}
+
+	public Person getNewLoginPerson(HttpServletRequest request, String id) {
+		Person person = new Person();
+		person.setAccount(id);
+		person.setDateCreated(null);
+
+		person.setDateLastActived(null);
+		person.setDeleted(false);
+		person.setIpCreated(request.getRemoteAddr());
+		person.setIpLastActived(request.getRemoteAddr());
+		person.setType(PersonType.STUDENT.toString());
+		return person;
+	}
+
+	public User createUserOnRemoteServer(String id) {
+		User user = null;
+		try {
+			HttpURLConnection connectionURL = connection.doConnection(new URL(SERVICEURL), UserConverter.convert(new User(id)), Connection.POST);
+			int status = connectionURL.getResponseCode();
+			connectionURL.connect();
+			if (status == 200) {
+				user = UserConverter.convert(connection.getStringFromConnection(connectionURL));
+			}
+		} catch (IOException e) {
+			user = null;
+		}
+		return user;
+	}
 
 }
