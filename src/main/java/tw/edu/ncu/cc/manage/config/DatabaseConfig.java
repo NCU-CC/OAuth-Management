790fe3331e7abbf5aa85d7fa03350ea79d871c2d
Yeh-Yung <yyc1217@gmail.com>
2015-03-27 17:20:13 +0800

    backup

--- src/main/java/tw/edu/ncu/cc/manage/config/DatabaseConfig.java
+++ src/main/java/tw/edu/ncu/cc/manage/config/DatabaseConfig.java
@@ -1,43 +1,103 @@
 package tw.edu.ncu.cc.manage.config;
 
-import javax.activation.DataSource;
+import java.util.Properties;
+
+import javax.persistence.EntityManagerFactory;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.AdviceMode;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
+import org.springframework.dao.annotation.PersistenceExceptionTranslationPostProcessor;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
 
-//@Configuration
-//@PropertySource("classpath:database.properties")
+@Configuration
+@EnableTransactionManagement(mode = AdviceMode.PROXY)
+@PropertySource("classpath:database.properties")
 public class DatabaseConfig {
 
 	@Autowired
 	private Environment env;
 	
-	@Bean
-	public HikariDataSource datasource() {
-	    HikariConfig hikariConfig = new HikariConfig();
-	    hikariConfig.setDriverClassName("com.mysql.jdbc.Driver");
-	    hikariConfig.setJdbcUrl("jdbc:mysql://localhost:3306/spring-test"); 
-	    hikariConfig.setUsername(env.getRequiredProperty("username"));
-	    hikariConfig.setPassword(env.getRequiredProperty("password"));
-
-	    hikariConfig.setMaximumPoolSize(5);
-	    hikariConfig.setConnectionTestQuery("SELECT 1");
-	    hikariConfig.setPoolName("springHikariCP");
-
-	    hikariConfig.addDataSourceProperty("dataSource.cachePrepStmts", "true");
-	    hikariConfig.addDataSourceProperty("dataSource.prepStmtCacheSize", "250");
-	    hikariConfig.addDataSourceProperty("dataSource.prepStmtCacheSqlLimit", "2048");
-	    hikariConfig.addDataSourceProperty("dataSource.useServerPrepStmts", "true");
-
-	    HikariDataSource dataSource = new HikariDataSource(hikariConfig);
+	@Bean(destroyMethod = "close")
+	public HikariDataSource dataSource() {
+	    
+	    HikariDataSource dataSource = new HikariDataSource(hikariConfig());
 
 	    return dataSource;
 	}
+	
+	@Bean HikariConfig hikariConfig() {
+		HikariConfig hikariConfig = new HikariConfig();
+		
+	    hikariConfig.setPoolName("springHikariCP");
+	    hikariConfig.setConnectionTestQuery("SELECT 1");
+	    hikariConfig.setMaximumPoolSize(100);
+	    hikariConfig.setMinimumIdle(10);
+	    hikariConfig.setIdleTimeout(600000);
+	    hikariConfig.setDataSourceClassName(env.getRequiredProperty("jdbc.driver"));
+
+	    hikariConfig.addDataSourceProperty("url", env.getRequiredProperty("jdbc.url"));
+	    hikariConfig.addDataSourceProperty("user", env.getRequiredProperty("jdbc.username"));
+	    hikariConfig.addDataSourceProperty("password", env.getRequiredProperty("jdbc.password"));
+	    
+	    hikariConfig.addDataSourceProperty("cachePrepStmts", "true");
+	    hikariConfig.addDataSourceProperty("prepStmtCacheSize", "250");
+	    hikariConfig.addDataSourceProperty("prepStmtCacheSqlLimit", "2048");
+	    hikariConfig.addDataSourceProperty("useServerPrepStmts", "true");
+	    hikariConfig.addDataSourceProperty("useUnicode", "true");
+	    hikariConfig.addDataSourceProperty("characterEncoding", "utf8");
+	    
+	    return hikariConfig;
+	}
+	
+	@Bean
+	public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+
+		LocalContainerEntityManagerFactoryBean emf = new LocalContainerEntityManagerFactoryBean();
+		emf.setDataSource(dataSource());
+		emf.setPackagesToScan(new String[] { "tw.edu.ncu.edu.manage.entity"});
+		
+		JpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+		emf.setJpaVendorAdapter(vendorAdapter);
+		emf.setJpaProperties(jpaProperties());
+
+		return emf;
+	}
+	
+	private Properties jpaProperties() {
+		Properties properties = new Properties();
+
+		properties.setProperty("hibernate.hbm2ddl.auto", "update");
+		properties.setProperty("hibernate.dialect", "org.hibernate.dialect.SQLServer2008Dialect");
+		properties.setProperty("hibernate.show_sql", "false");
+		properties.setProperty("hibernate.ejb.naming_strategy", "org.hibernate.cfg.ImprovedNamingStrategy");
+		
+		return properties;
+	}
+
+	@Bean
+	public PlatformTransactionManager transactionManager(EntityManagerFactory emf) {
+		JpaTransactionManager transactionManager = new JpaTransactionManager();
+		transactionManager.setEntityManagerFactory(emf);
+
+		return transactionManager;
+	}
+
+	@Bean
+	public PersistenceExceptionTranslationPostProcessor exceptionTranslation() {
+		return new PersistenceExceptionTranslationPostProcessor();
+	}
+
 	
 }
---
+++ src/main/java/tw/edu/ncu/cc/manage/config/ViewConfig.java
@@ -0,0 +1,39 @@
+package tw.edu.ncu.cc.manage.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+import org.springframework.web.servlet.view.UrlBasedViewResolver;
+import org.springframework.web.servlet.view.tiles3.TilesConfigurer;
+import org.springframework.web.servlet.view.tiles3.TilesView;
+
+@Configuration
+public class ViewConfig {
+
+	@Bean
+	public UrlBasedViewResolver tilesViewResolver() {
+		UrlBasedViewResolver resolver = new UrlBasedViewResolver();
+		resolver.setViewClass(TilesView.class);
+		resolver.setOrder(1);
+		return resolver;
+
+	}
+
+	@Bean
+	public TilesConfigurer tilesConfigurer() {
+		TilesConfigurer configurer = new TilesConfigurer();
+		configurer.setDefinitions("/WEB-INF/layouts/layouts.xml", "/WEB-INF/views/**/views.xml");
+		return configurer;
+	}
+
+	@Bean
+	public InternalResourceViewResolver jspViewResolver() {
+		InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+		resolver.setViewClass(JstlView.class);
+		resolver.setPrefix("/WEB-INF/views/");
+		resolver.setSuffix(".jsp");
+		resolver.setOrder(2);
+		return resolver;
+	}
+}
