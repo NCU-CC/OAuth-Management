6a31d0c284847904aadb57820339b2da5fef6abb
Yeh-Yung <yyc1217@gmail.com>
2015-04-07 15:50:22 +0800

    person mapping OK.

--- src/main/java/tw/edu/ncu/cc/manage/controller/auth/AfterLoginController.java
+++ src/main/java/tw/edu/ncu/cc/manage/controller/auth/AfterLoginController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import tw.edu.ncu.cc.manage.entity.Person;
-import tw.edu.ncu.cc.manage.service.login.IPersonService;
+import tw.edu.ncu.cc.manage.service.IPersonService;
 import tw.edu.ncu.cc.manage.util.PersonInfo;
 
 @Controller
--- src/main/java/tw/edu/ncu/cc/manage/dao/AbstractDao.java
+++ src/main/java/tw/edu/ncu/cc/manage/dao/AbstractDao.java
@@ -1,58 +1,37 @@
 package tw.edu.ncu.cc.manage.dao;
 
-import java.util.Collections;
-import java.util.List;
 import java.util.Optional;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import javax.persistence.Query;
-
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class AbstractDao<T> implements IDao<T> {
+public abstract class AbstractDao {
 	
-	@PersistenceContext
-	private EntityManager entityManager;
+	@Autowired
+    private SessionFactory sessionFactory;
+ 
+	
+    protected Session getSession() {
+        return sessionFactory.getCurrentSession();
+    }
 
-	public Optional<T> find(Class<T> clazz, int id) {
-		return Optional.ofNullable(this.entityManager.find(clazz, id));
+	public Optional<Object> find(Class<?> clazz, int id) {
+		return Optional.ofNullable(getSession().get(clazz, id));
 	}
 
-	public void create(T t) {
-		this.entityManager.persist(t);
+	public void create(Object entity) {
+		this.getSession().persist(entity);
 	}
 
-	public Optional<T> save(T t) {
-		return Optional.ofNullable(this.entityManager.merge(t));
+	public Optional<Object> save(Object entity) {
+		return Optional.ofNullable(this.getSession().save(entity));
 	}
 
-	public void delete(T t) {
-		this.entityManager.remove(t);
-	}
-
-	public int count(String hql, Object... params) {
-		Query query = createQuery(hql);
-		for (int i = 0; params != null && i < params.length; i++) {
-			query.setParameter(i + 1, params[i]);
-		}
-		Object object = createQuery(hql).getFirstResult();
-		return ((Long) object).intValue();
-	}
-
-	@SuppressWarnings("unchecked")
-	public List<T> list(String hql, int firstResult, int maxSize, Object... params) {
-		Query query = createQuery(hql);
-		for (int i = 0; params != null && i < params.length; i++) {
-			query.setParameter(i + 1, params[i]);
-		}
-		List<T> list = createQuery(hql).setFirstResult(firstResult).setMaxResults(maxSize).getResultList();
-		return Optional.ofNullable(list).orElse(Collections.emptyList());
-	}
-
-	public Query createQuery(String hql) {
-		return entityManager.createQuery(hql);
+	public void delete(Object entity) {
+		this.getSession().delete(entity);
 	}
 
 }
