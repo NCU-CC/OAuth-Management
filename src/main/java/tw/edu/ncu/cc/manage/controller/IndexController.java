e724b58d77b95d74016f8c95e718980e828cb5e9
Yeh-Yung <yyc1217@gmail.com>
2015-04-20 22:33:06 +0800

    add Person object to session

--- src/main/java/tw/edu/ncu/cc/manage/controller/IndexController.java
+++ src/main/java/tw/edu/ncu/cc/manage/controller/IndexController.java
@@ -1,7 +1,12 @@
 package tw.edu.ncu.cc.manage.controller;
 
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+
+import tw.edu.ncu.cc.manage.utils.SystemConstant;
 
 /**
  * 首頁
@@ -9,10 +14,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
  *
  */
 @Controller
+@SessionAttributes(SystemConstant.USER)
 public class IndexController {
 
     @RequestMapping({"/", "/index", "/default"})
-    public String index() {                
+    public String index(Model model) {
+    	if (!model.containsAttribute(SystemConstant.USER)) {
+    		model.addAttribute(SystemConstant.USER, SecurityContextHolder.getContext().getAuthentication().getPrincipal());
+    	}
         return "index";
     }
 }
---
+++ src/main/java/tw/edu/ncu/cc/manage/utils/SystemConstant.java
@@ -0,0 +1,5 @@
+package tw.edu.ncu.cc.manage.utils;
+
+public class SystemConstant {
+	public static final String USER = "_user";
+}
