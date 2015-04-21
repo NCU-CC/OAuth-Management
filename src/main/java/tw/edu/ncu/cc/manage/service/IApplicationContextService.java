1b2c28dc3eae5ebf6308acbf516c8c5637a51c14
center56 <center56@cc.ncu.edu.tw>
2015-04-21 11:41:53 +0800

    add Person object to session.

---
+++ src/main/java/tw/edu/ncu/cc/manage/service/IApplicationContextService.java
@@ -0,0 +1,7 @@
+package tw.edu.ncu.cc.manage.service;
+
+import tw.edu.ncu.cc.manage.entity.Person;
+
+public interface IApplicationContextService {
+	public Person getCurrentUser();
+}
---
+++ src/main/java/tw/edu/ncu/cc/manage/service/impl/ApplicationContextService.java
@@ -0,0 +1,17 @@
+package tw.edu.ncu.cc.manage.service.impl;
+
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+
+import tw.edu.ncu.cc.manage.entity.Person;
+import tw.edu.ncu.cc.manage.service.IApplicationContextService;
+
+@Service
+public class ApplicationContextService implements IApplicationContextService {
+
+	@Override
+	public Person getCurrentUser() {
+		return (Person) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+	}
+
+}
