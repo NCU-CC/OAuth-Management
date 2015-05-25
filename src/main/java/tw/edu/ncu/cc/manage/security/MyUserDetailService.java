4120432689fb4be4d89ab76441f021b5d4db9a29
center56 <center56@cc.ncu.edu.tw>
2015-05-25 16:51:36 +0800

    登入ok

--- src/main/java/tw/edu/ncu/cc/manage/security/MyUserDetailService.java
+++ src/main/java/tw/edu/ncu/cc/manage/security/MyUserDetailService.java
@@ -1,8 +1,12 @@
 package tw.edu.ncu.cc.manage.security;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,20 +16,25 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.openid.OpenIDAttribute;
 import org.springframework.security.openid.OpenIDAuthenticationToken;
 import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
 import tw.edu.ncu.cc.manage.config.SecurityConfig;
 import tw.edu.ncu.cc.manage.entity.Person;
 import tw.edu.ncu.cc.manage.enums.RoleEnum;
 import tw.edu.ncu.cc.manage.service.IPersonService;
 
+import com.google.inject.internal.Lists;
+
 @Service
 public class MyUserDetailService implements AuthenticationUserDetailsService<OpenIDAuthenticationToken> {
 
 	private static final Logger logger = LoggerFactory.getLogger(MyUserDetailService.class);
-	
+
 	@Autowired
 	private IPersonService personService;
-	
+
 	/**
 	 * 允許存取本系統的role
 	 */
@@ -33,56 +42,89 @@ public class MyUserDetailService implements AuthenticationUserDetailsService<Ope
 
 	@Override
 	public UserDetails loadUserDetails(OpenIDAuthenticationToken token) throws UsernameNotFoundException, NoSuchUserRoleException {
-		
+
 		logger.debug("OpenID token {}", token);
-		checkRoleAvailibility(token);
-		
-		String account = ((String) token.getPrincipal()).substring(SecurityConfig.PORTAL_ENDPOINT.length());
+
+		if (!isAvailableRole(token)) {
+			throw new NoSuchUserRoleException(token, PERMIT_ROLES);
+		}
+
+		String account = StringUtils.substringAfterLast((String) token.getPrincipal(), "/");
+
+		if (StringUtils.isEmpty(account)) {
+			throw new UsernameNotFoundException(token.toString());
+		}
 		
 		Optional<Person> person = this.personService.findByAccount(account);
 		if (person.isPresent()) {
-			logger.debug("使用者已註冊，跳過此步驟");
+			logger.debug("使用者已註冊，回傳已儲存資料");
+			this.personService.refreshLastIp(person.get(), ip());
 			return person.get();
 		}
-		
-		throw new OpenIDUserNotFoundException("找不到使用者帳號 " + account, token);
+
+		logger.debug("使用者未註冊，產生新資料");
+
+		Person newPerson = new Person(account, roles(token));
+		newPerson.setIpCreated(ip());
+		newPerson.setIpLastActived(ip());
+		this.personService.create(newPerson);
+		// TODO publish new user event to oauth service
+		return newPerson;
 	}
 
 	/**
-	 * 確認該使用者的role是有權限來access本系統
-	 * @param token
+	 * 使用者ip
+	 * @return
 	 */
-	private void checkRoleAvailibility(OpenIDAuthenticationToken token) {
-		
-		List<OpenIDAttribute> attributes = token.getAttributes();
-		for (OpenIDAttribute attribute : attributes) {
-			if (isRoleAttribute(attribute)) {
-				logger.debug("使用者 OpenID roles: {}", attribute);
-				if (noSuchRoles(attribute)) {
-					throw new NoSuchUserRoleException(token, PERMIT_ROLES);
-				}
-				token.setDetails(attribute.getValues());
-			}
-		}
+	private String ip() {
+		ServletRequestAttributes sra = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+	    HttpServletRequest req = sra.getRequest();
+	    return req.getRemoteAddr();
 	}
 	
 	/**
+	 * 該使用者的role有權限access本系統
+	 * 
+	 * @param token
+	 */
+	private boolean isAvailableRole(OpenIDAuthenticationToken token) {
+		return CollectionUtils.containsAny(PERMIT_ROLES, roles(token));
+	}
+
+	/**
+	 * 提取角色屬性
+	 * 
+	 * @param token
+	 * @return
+	 */
+	private List<String> roles(OpenIDAuthenticationToken token) {
+		List<OpenIDAttribute> attributes = token.getAttributes();
+
+		for (OpenIDAttribute attribute : attributes) {
+
+			if (isRoleAttribute(attribute)) {
+
+				logger.debug("使用者 OpenID roles: {}", attribute);
+
+				String roles = attribute.getValues().get(0);
+				roles = StringUtils.remove(roles, "\"");
+				roles = StringUtils.remove(roles, "[");
+				roles = StringUtils.remove(roles, "]");
+
+				return Lists.newArrayList(roles.split(","));
+			}
+		}
+		return Collections.emptyList();
+	}
+
+	/**
 	 * 這個attribute是屬性role
+	 * 
 	 * @param attribute
 	 * @return
 	 */
 	private boolean isRoleAttribute(OpenIDAttribute attribute) {
 		return SecurityConfig.AX_NAME_ROLE.equals(attribute.getName());
 	}
-	
-	/**
-	 * 沒有本系統可允許存取的role
-	 * @param attribute
-	 * @return
-	 */
-	private boolean noSuchRoles(OpenIDAttribute attribute) {
-		String roles = attribute.getValues().get(0);
-		boolean hasAnyRole = PERMIT_ROLES.stream().anyMatch(role -> roles.indexOf(role) > -1);
-		return !hasAnyRole;
-	}
+
 }
