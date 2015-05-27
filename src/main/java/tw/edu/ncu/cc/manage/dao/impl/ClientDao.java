0d2d4e5aac67cdfcb762f16989a5d4dbbb323a7c
Yeh-Yung <yyc1217@gmail.com>
2015-05-27 20:39:03 +0800

    change Application to Client

---
+++ src/main/java/tw/edu/ncu/cc/manage/dao/impl/ClientDao.java
@@ -0,0 +1,51 @@
+package tw.edu.ncu.cc.manage.dao.impl;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Repository;
+import org.springframework.util.Assert;
+
+import tw.edu.ncu.cc.manage.dao.IClientDao;
+import tw.edu.ncu.cc.manage.dao.support.AbstractOAuthServiceDao;
+import tw.edu.ncu.cc.manage.domain.Client;
+
+@Repository
+public class ClientDao extends AbstractOAuthServiceDao<Client> implements IClientDao {
+	
+	@Override
+	public List<Client> findAll(String username) {
+		Assert.hasText(username);
+		return getList(userUrl + username + "/clients");
+	}
+
+	@Override
+	public Optional<Client> find(String clientId){
+		Assert.hasText(clientId);
+		return get(clientUrl + clientId);
+	}
+
+	@Override
+	public Client update(Client client){
+		Assert.notNull(client);
+		return put(clientUrl + client.getId(), client);
+	}
+
+	@Override
+	public Client create(Client client){
+		Assert.notNull(client);
+		return post(clientUrl, client);
+	}
+
+	@Override
+	public void remove(Client client) {
+		Assert.notNull(client);
+		delete(clientUrl + client.getId());
+	}
+
+	@Override
+	public Client refreshSecret(String clientId) {
+		Assert.hasText(clientId);
+		return post(clientUrl + clientId + "/secret/", null);
+	}
+}
