5bf30f4b4c8b5361a705a2a4582c2a63468d5205
Yeh-Yung <yyc1217@gmail.com>
2015-07-16 15:01:03 +0800

    測試機不用https

--- src/integration-test/java/tw/edu/ncu/cc/manage/dao/impl/utils/DaoTestUtils.java
+++ src/integration-test/java/tw/edu/ncu/cc/manage/dao/impl/utils/DaoTestUtils.java
@@ -3,7 +3,7 @@ package tw.edu.ncu.cc.manage.dao.impl.utils;
 import tw.edu.ncu.cc.manage.domain.Client;
 
 public class DaoTestUtils {
-	public static final String ROOT_URL = "https://140.115.3.188/oauth/management/v1/";
+	public static final String ROOT_URL = "http://140.115.3.188/oauth/management/v1/";
 	public static final String FAKE_USER_NAME = "VERIFY_TEST_USER_NAME";
 	public static final String FAKE_NAME = "VERIFY_TEST_NAME";
 	
